2034bdd01748eb3763c8f41bac2df5d9f58d0717
prepare <57052177+zcdsg125@users.noreply.github.com>
2023-06-27 10:32:00 +0800

    新增员工

--- src/main/java/com/it/regwm/controller/EmpolyeeController.java
+++ src/main/java/com/it/regwm/controller/EmpolyeeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.invoke.LambdaConversionException;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -56,5 +57,20 @@ public class EmpolyeeController {
         request.getSession().removeAttribute("employee");
         return R.success("账号已退出");
 
+    }
+
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("新增员工，员工信息：{}",employee.toString());
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        empolyService.save(employee);
+
+        return R.success("新增员工成功");
     }
 }
